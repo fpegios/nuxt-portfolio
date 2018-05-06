f1acdfd1b0c9998a3146d3c7791a1f23fafc47da
Fotis Pegios <fotispeg@gmail.com>
2018-05-06 18:16:41 +0200

    create portfolio

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">About Me</h1>
+      <p>
+        My name is Fotis Pegios (AKA fpegios), I’m a Front-End Developer at Golden Arm Inc and MSc Student in Digital Media Engineering at DTU.
+      </p>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Fotis Pegios</h1>
+      <h2 class="subtitle">a Software Engineer based in Copenhagen, Denmark.</h2>
+    </div>
+  </section>
+</template>
